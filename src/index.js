6141ecbf14f46bbeef2b14d3a03e0e1660cdc721
sir-yegor <yegor.s@radency.com>
2024-04-03 16:55:05 +0300

    feat/routes to fetch all the routes and by specific user

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const productRoutes = require('./product.routes');
 const { logRequest } = require('./middleware');
 const { errorResponder } = require('./error.middleware');
 const userRouter = require('./user/user.router');
+const productRouter = require('./product/product.router');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -13,8 +14,10 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(logRequest);
 app.use(productRoutes);
-app.use(errorResponder);
 app.use(userRouter);
+app.use(productRouter);
+app.use(errorResponder);
+
 
 app.listen(PORT, () => {
    console.log(`Server listening at http://localhost:${PORT}`);
